65fe888308a697de81258b30b245820db0b025a4
GustavoDimeira <gustavodimeira1@gmail.com>
2023-05-25 16:01:23 -0400

    feat: tests ingredients

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,11 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
-
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-    pass
+    bacon = Ingredient("bacon")
+
+    assert bacon.name == "bacon"
+    assert bacon.restrictions == {Restriction.ANIMAL_MEAT, Restriction.ANIMAL_DERIVED}
+    assert bacon.__repr__() == f"Ingredient('bacon')"
+    assert bacon.__eq__(bacon)
+    assert bacon.__hash__() == hash("bacon")
