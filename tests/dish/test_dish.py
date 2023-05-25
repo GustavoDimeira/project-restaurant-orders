ec35a6d4d8a3e7628dfa27a294789acc735bf686
GustavoDimeira <gustavodimeira1@gmail.com>
2023-05-25 16:59:27 -0400

    feat: tests dish

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,22 @@
 from src.models.dish import Dish  # noqa: F401, E261, E501
-
+import pytest
 
 # Req 2
 def test_dish():
-    pass
+    prato = Dish("prato", 10)
+
+    assert prato.name == "prato"
+    print(prato.get_restrictions())
+    assert prato.__repr__() == f"Dish('prato', R$10.00)"
+    assert prato.__eq__(prato)
+    assert prato.__hash__() == hash("Dish('prato', R$10.00)")
+    assert prato.get_restrictions() == set()
+
+    prato.add_ingredient_dependency("presunto", 10)
+    assert prato.get_ingredients() == set({"presunto"})
+
+    with pytest.raises(ValueError):
+        Dish("valueError", -1)
+    
+    with pytest.raises(TypeError):
+        Dish("valueError", "sqrt(2)")
